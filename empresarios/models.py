2d472859514aec72b82223389c62ecfd1ca07600
suricatstar <caue.tamiarana@gmail.com>
2024-08-09 23:16:13 -0300

    feita a parte de empresas

--- empresarios/models.py
+++ empresarios/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Empresas(models.Model):
@@ -39,3 +41,29 @@ class Empresas(models.Model):
 
     def __str__(self):
         return f'{self.user.username} | {self.nome}'
+    
+    @property
+    def status(self):
+        if date.today() > self.data_final_captacao:
+            return mark_safe('<span class="badge bg-success">Captação finalizada</span>')
+        return mark_safe('<span class="badge bg-primary">Em captação</span>')
+    
+    @property
+    def valuation(self):
+        return f'{(100 * self.valor) / self.percentual_equity:.2f}' 
+    
+class Documento(models.Model):
+    empresa = models.ForeignKey(Empresas, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=30)
+    arquivo = models.FileField(upload_to="documentos")
+
+    def __str__(self):
+        return self.titulo
+    
+class Metricas(models.Model):
+    empresa = models.ForeignKey(Empresas, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=30)
+    valor = models.FloatField()
+
+    def __str__(self):
+        return self.titulo
